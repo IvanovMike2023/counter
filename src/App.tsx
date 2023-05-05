60624f3d024783d9fc6a3d9967e84539ab5a7f92
Ivanov273 <Ivanov.Michail.2018@gmail.com>
2023-05-05 08:22:14 +0300

    '1'

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+
+export type FilterValuesType = "all" | "active" | "completed";
+
+function App() {
+    return (
+        <div className="App">
+            cadcasc
+        </div>
+    );
+}
+
+export default App;
